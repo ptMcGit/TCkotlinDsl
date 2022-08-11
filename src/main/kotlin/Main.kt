41016d9fc2507e822a6bd011d659887c826dc095
PM <13983935+ptMcGit@users.noreply.github.com>
2022-08-11 10:21:58 -0400

    IDE made updates, update gitignore, add dependencies
    - who knows what the IDE updated
    - .gitignore to ignore intellij,gradle files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package com.github.ptmcgit.tckotlindsl
 
+import jetbrains.buildServer.configs.kotlin.*
+
 fun hello() {
     println("Hello World!")
+}
+
+project {
+    description = "For playing around with Kotlin DSL."
 }
